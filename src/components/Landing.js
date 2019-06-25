50fe32a257ebd889dc35d4d848b09c9288192500
wickdeeb <isak.m.h.s@gmail.com>
2019-06-25 12:42:18 +0200

    initial commit

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
+
+function Landing() {
+  return(
+    <div className="landing-container">
+      Adidas Skateboarding
+      <ScrollAnimation animateIn="flash">
+        test
+      </ScrollAnimation>
+    </div>
+  );
+}
+
+export default Landing;
